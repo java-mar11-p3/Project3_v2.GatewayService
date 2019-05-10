b7459b24c0e244134b4554ff2818c466ee60f8b6
poholo502 <poholo502@gmail.com>
2019-05-10 17:23:49 -0500

    finish condumentation

--- src/main/java/com/java/GatewayService.java
+++ src/main/java/com/java/GatewayService.java
@@ -4,7 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-
+/**
+ * 
+ * @author Poho
+ * This class is to register API Gateway as client in Eureka so it can hand requests from frontend
+ * and filter requests to applicable services on the Eureka server
+ *  
+ */
 @SpringBootApplication
 @EnableEurekaClient
 @EnableZuulProxy
